3c3279491c8168133ec7ebd18dc04b70eb82cd97
Alexandre BOIN <boin.alexandre@wanadoo.fr>
2020-11-24 17:24:47 +0100

    cloudinary and indexpage

--- app/controllers/hauntedhouses_controller.rb
+++ app/controllers/hauntedhouses_controller.rb
@@ -1,35 +1,35 @@
 class HauntedhousesController < ApplicationController
-       
-    before_action :set_hauntedhouse, only: [:show]
-    
-    def index
-         @hauntedhouses = Hauntedhouse.all
-    end
-   
-    def new
-        @hauntedhouse = Hauntedhouse.new
-    end
-    # ​  
-    def show
-        
-    end
-    # ​
-       def create
-         @hauntedhouse = Hauntedhouse.new(hauntedhouse_params)
-         if @hauntedhouse.save
-           redirect_to hauntedhouses_path
-         else
-          render :new
-        end
-       end
+  before_action :set_hauntedhouse, only: [:show]
 
-private
-    def hauntedhouse_params
-        params.require(:hauntedhouse).permit(:name, :description, :category, :address, :city, :country, :price_per_night)
-    end
+  def index
+    @hauntedhouses = Hauntedhouse.all
+  end
 
-    def set_hauntedhouse
-        @hauntedhouse = Hauntedhouse.find(params[:id])
-    end
+  def new
+    @hauntedhouse = Hauntedhouse.new
+  end
 
+  # ​
+  def show
+  end
+
+  # ​
+  def create
+    @hauntedhouse = Hauntedhouse.new(hauntedhouse_params)
+    if @hauntedhouse.save
+      redirect_to hauntedhouses_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def hauntedhouse_params
+    params.require(:hauntedhouse).permit(:name, :description, :category, :address, :city, :country, :price_per_night, :photo)
+  end
+
+  def set_hauntedhouse
+    @hauntedhouse = Hauntedhouse.find(params[:id])
+  end
 end
--- app/models/hauntedhouse.rb
+++ app/models/hauntedhouse.rb
@@ -1,4 +1,5 @@
 class Hauntedhouse < ApplicationRecord
   has_many :reviews
   has_many :journeys
+  has_one_attached :photo
 end
