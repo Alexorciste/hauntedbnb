938fac1a4bc528311b2397322e462b398ce419e7
julien daniel <j.delarita@gmail.com>
2020-11-25 16:13:45 +0100

    journey

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'hauntedhouses#index'
   resources :hauntedhouses, only: [:index, :show, :new, :edit, :update, :create ] do
-    resources :journeys, only: [:new, :create]
+    resources :journeys, only: [:index, :show, :new, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
